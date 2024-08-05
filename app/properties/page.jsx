90e404e5b69bef56bbf16ea9326d9e5edb514714
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-05 20:26:42 +0430

    add property items

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,12 @@
 import Link from "next/link"
+import PropertyAddPage from "./add/page"
 
 const PropertiesPage = ()=>  {
   return (
      <div>
-            <h1 className="text-3xl">Properties</h1>
-            <Link href="/">Go Home</Link>
+        <h1 className="text-3xl">Properties</h1>
+        <Link href="/">Go Home</Link>
+        <PropertyAddPage />
         </div>
   )
 }
