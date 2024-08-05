17171e329f3038fe456f0020cf7a7ecbaf984e5f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-05 20:26:49 +0430

    add property items

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,6 +1,19 @@
+import PropertyCard from '@/app/components/PropertyCard'
+import properties from '@/properties.json'
+
 function PropertyAddPage() {
   return (
-    <div>PropertyAddPage</div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? <p>No properties found</p> : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.map((property) => {
+            <PropertyCard key={property._id} property={property} />
+          })}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
