121f8a6affda2e96f5fb080b991433596a5fac8d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-10 05:00:24 +0430

    adds home properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 import Hero from "./components/Hero"
+import HomeProperties from "./components/HomeProperties"
 import InfoBoxes from "./components/InfoBoxes"
+
 
 const HomePage = () => {
     return (
         <>
             <Hero />
             <InfoBoxes />
+            <HomeProperties/>
         </>
   )
 }
