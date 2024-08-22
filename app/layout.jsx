12d703e91b983c7f5248786e0fbec920118a88f7
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-22 20:01:33 +0430

    moving file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import '@/assets/styles/globals.css'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 
 export const metadata = {
   title: "Properties | Dind your Dental",
