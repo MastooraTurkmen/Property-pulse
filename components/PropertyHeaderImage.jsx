98aa008f943ba4ca2d92fc878215efccd85c128c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-23 20:16:31 +0430

    property header

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -1,6 +1,21 @@
-const PropertyHeaderImage = () => {
+import Image from "next/image"
+
+const PropertyHeaderImage = ({image}) => {
   return (
-    <div>PropertyHeaderImage</div>
+  <section>
+        <div className="container-xl m-auto">
+          <div className="grid grid-cols-1">
+            <Image
+              src={`/images/properties/${image}`}
+              alt=""
+              className="object-cover h-[400px] w-full"
+              height={0}
+              width={0}
+              sizes="100vw"
+            />
+          </div>
+        </div>
+    </section>
   )
 }
 
