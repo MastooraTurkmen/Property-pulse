77e5afbaf0bbaa8e71a96a870bb9b44265c0fc80
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-04 22:04:04 +0430

    auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,24 @@
-import '@/assets/styles/globals.css'
-import Navbar from '../components/Navbar'
+import "@/assets/styles/globals.css";
+import Navbar from "../components/Navbar";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Properties | Dind your Dental",
   description: "Find your dream rental property",
-  keywords: "rental, find rentals, find properties"
-}
+  keywords: "rental, find rentals, find properties",
+};
 
 function MainLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
-  )
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+        </body>
+      </html>
+    </AuthProvider>
+  );
 }
 
-export default MainLayout
+export default MainLayout;
