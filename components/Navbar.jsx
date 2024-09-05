8f922a6869dbe825096479ec40053b42372e3517
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-05 20:48:08 +0430

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -188,6 +188,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -197,6 +200,9 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
@@ -204,6 +210,10 @@ const Navbar = () => {
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
+                      onClick={() => {
+                        setIsMobileMenuOpen(false);
+                        signOut();
+                      }}
                       id="user-menu-item-2"
                     >
                       Sign Out
