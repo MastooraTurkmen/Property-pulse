982a3c4a467d5311d19502a2afcedbd7f46f3ecf
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-07 20:25:17 +0430

    route js

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Properties";
+import { getSessionUser } from "@/utils/getSessionUser";
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -14,5 +15,29 @@ export const GET = async (request) => {
         return new Response('Something Went Wrong', {
             status: 500
         })
+    }
+}
+
+export const POST = async (request) => {
+    try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+
+        if (!sessionUser || !sessionUser.userId) {
+            return new Response('User ID is required', { status: 401 })
+        }
+
+        const { userId } = sessionUser;
+
+        const formData = await request.formData();
+        const amenities = formData.getAll('amenities')
+        const images = formData.getAll('images').filter((image) => image.name !== '')
+
+        return new Response(JSON.stringify({
+            message: 'Success'
+        }), { status: 200 })
+    } catch (error) {
+        return new Response('Failed to add property', { status: 500 })
     }
 }
