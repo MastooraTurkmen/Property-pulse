b465c071419579ed01a75011e0b585dafa9d8b66
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-17 20:16:32 +0430

    profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,76 @@
-import { useSession } from "next-auth/react";
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 import profileDefault from "@/assets/images/profile.png";
+import Spinner from "@/components/Spinner";
+import { toast } from "react-toastify";
 
-const ProfilePage = async () => {
+const ProfilePage = () => {
   const { data: session } = useSession();
   const profileImage = session?.user?.image;
   const profileName = session?.user?.name;
   const profileEmail = session?.user?.email;
+
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserProperties = async (userId) => {
+      if (!userId) {
+        return;
+      }
+
+      try {
+        const res = await fetch(`/api/properties/user/${userId}`);
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setProperties(data);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // Fetch user properties when session is available
+    if (session?.user?.id) {
+      fetchUserProperties(session.user.id);
+    }
+  }, [session]);
+
+  const handleDeleteProperty = async (propertyId) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this property?"
+    );
+
+    if (!confirmed) return;
+
+    try {
+      const res = await fetch(`/api/properties/${propertyId}`, {
+        method: "DELETE",
+      });
+
+      if (res.status === 200) {
+        // Remove the property from state
+        const updatedProperties = properties.filter(
+          (property) => property._id !== propertyId
+        );
+
+        setProperties(updatedProperties);
+
+        toast.success("Property Deleted");
+      } else {
+        toast.error("Failed to delete property");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete property");
+    }
+  };
 
   return (
     <section className="bg-blue-50">
@@ -18,7 +82,7 @@ const ProfilePage = async () => {
               <div className="mb-4">
                 <Image
                   className="h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0"
-                  src={profileDefault || profileImage}
+                  src={profileImage || profileDefault}
                   width={200}
                   height={200}
                   alt="User"
@@ -34,6 +98,49 @@ const ProfilePage = async () => {
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
+              {!loading && properties.length === 0 && (
+                <p>You have no property listings</p>
+              )}
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                properties.map((property) => (
+                  <div key={property._id} className="mb-10">
+                    <Link href={`/properties/${property._id}`}>
+                      <Image
+                        className="h-32 w-full rounded-md object-cover"
+                        src={property.images[0]}
+                        alt=""
+                        width={500}
+                        height={100}
+                        priority={true}
+                      />
+                    </Link>
+                    <div className="mt-2">
+                      <p className="text-lg font-semibold">{property.name}</p>
+                      <p className="text-gray-600">
+                        Address: {property.location.street}{" "}
+                        {property.location.city} {property.location.state}
+                      </p>
+                    </div>
+                    <div className="mt-2">
+                      <Link
+                        href={`/properties/${property._id}/edit`}
+                        className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => handleDeleteProperty(property._id)}
+                        className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+                        type="button"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
