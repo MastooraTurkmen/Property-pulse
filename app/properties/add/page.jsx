ac51ec7db9e547372937f72d938988ca898ae905
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-17 20:16:51 +0430

    property

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,20 +1,14 @@
-import PropertyCard from '@/components/PropertyCard'
-import properties from '@/properties.json'
+import PropertyAddForm from "@/components/PropertyAddForm";
 
-function PropertyAddPage() {
+const PropertyAddPage = () => {
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? <p>No properties found</p> : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => {
-            <PropertyCard key={property._id} property={property} />
-          })}
-          </div>
-        )}
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
       </div>
     </section>
-  )
-}
-
-export default PropertyAddPage
+  );
+};
+export default PropertyAddPage;
