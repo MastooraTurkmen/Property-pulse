f636e9371006c736da122786bc570606f7ada019
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-17 20:15:00 +0430

    made some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,32 @@
-import "@/assets/styles/globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
+import "@/assets/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+// import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
-  title: "Properties | Dind your Dental",
+  title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
   keywords: "rental, find rentals, find properties",
 };
 
-function MainLayout({ children }) {
+const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <body>
-          <Navbar />
-          <main>{children}</main>
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang="en">
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
-}
-
+};
 export default MainLayout;
